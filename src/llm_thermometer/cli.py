8e4a36aa200a33657e74f9bc57a77ecd8cac7206
S1M0N38 <bertolottosimone@gmail.com>
2025-02-28 19:49:23 +0100

    feat(cli): update cli for file creation based on session

--- src/llm_thermometer/cli.py
+++ src/llm_thermometer/cli.py
@@ -3,9 +3,42 @@
 import argparse
 import asyncio
 import sys
+from argparse import Namespace
+from datetime import datetime
 from pathlib import Path
 
 from llm_thermometer import generate, measure
+
+TEMPERATURES = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+
+
+def cmd_generate(args: Namespace):
+    """Generate samples and save them into <data_dir>/samples"""
+
+    (args.data_dir / "samples").mkdir(exist_ok=True)
+    timestamp = datetime.now().strftime("%Y%m%dT%H%M%S")
+    args.output_file = args.data_dir / "samples" / f"{timestamp}.jsonl"
+
+    if args.temperature:
+        args.temperature = float(args.temperature)
+        asyncio.run(generate.generate_samples_and_save(args))
+    else:
+        for temperature in TEMPERATURES:
+            args.temperature = temperature
+            asyncio.run(generate.generate_samples_and_save(args))
+
+
+def cmd_measure(args: Namespace):
+    """Calculate similarities for all samples in <data_dir>/samples
+    and save them into <data_dir>/similarities"""
+
+    (args.data_dir / "similarities").mkdir(exist_ok=True)
+    for input_file in (args.data_dir / "samples").glob("*.jsonl"):
+        output_file = input_file.parent.parent / "similarities" / input_file.name
+        if not output_file.exists():
+            args.input_file = input_file
+            args.output_file = output_file
+            asyncio.run(measure.calculate_similarities_and_save(args))
 
 
 def main():
@@ -13,14 +46,18 @@ def main():
     parser = argparse.ArgumentParser(
         description="LLM Thermometer - Estimate temperature values of LLMs"
     )
-    subparsers = parser.add_subparsers(dest="command", required=True)
+    subparsers = parser.add_subparsers(
+        dest="command",
+        required=True,
+    )
 
     # Generate command
     generate_parser = subparsers.add_parser(
-        "generate", help="Generate multiple LLM samples"
+        name="generate",
+        help="Generate multiple LLM samples",
     )
     generate_parser.add_argument(
-        "--model",
+        "--language-model",
         type=str,
         required=True,
         help="Model to use for the LLM",
@@ -38,50 +75,50 @@ def main():
         help="Number of samples to generate",
     )
     generate_parser.add_argument(
-        "--output-file",
+        "--data-dir",
         type=Path,
         required=True,
-        help="Output file path (JSONL)",
+        help="Directory to save generated samples",
     )
     generate_parser.add_argument(
         "--temperature",
-        type=float,
-        default=None,
-        help="Temperature to use",
+        type=str,
+        default="",
+        help="Temperature to use in samples generation",
     )
 
-    # Generate command
+    # Measure command
     measure_parser = subparsers.add_parser(
-        "measure", help="Measure semantic similarity of samples"
+        name="measure",
+        help="Measure semantic similarity of samples",
     )
     measure_parser.add_argument(
-        "--model",
+        "--embedding-model",
         type=str,
         required=True,
         help="Embedding model for computing embeddings",
     )
     measure_parser.add_argument(
-        "--input-file",
+        "--data-dir",
         type=Path,
         required=True,
-        help="Input file path with samples (JSONL)",
-    )
-    measure_parser.add_argument(
-        "--output-file",
-        type=Path,
-        required=True,
-        help="Output file path with computed measures (JSONL)",
+        help="Directory with samples to measure",
     )
 
     args = parser.parse_args()
 
-    if args.command == "generate":
-        asyncio.run(generate.generate_samples_and_save(args))
-    elif args.command == "measure":
-        asyncio.run(measure.calculate_similarities_and_save(args))
-    else:
-        parser.print_help()
-        sys.exit(1)
+    assert args.data_dir.is_dir() and args.data_dir.exists()
+
+    match args.command:
+        case "generate":
+            cmd_generate(args)
+
+        case "measure":
+            cmd_measure(args)
+
+        case _:
+            parser.print_help()
+            sys.exit(1)
 
 
 if __name__ == "__main__":
